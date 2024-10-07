b8f7a6761f03574bc847937f2656eb602c20eae9
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-10-07 00:06:23 -0400

    tweaked my html and css files to prepare for my following code

--- scratch.mjs
+++ scratch.mjs
@@ -80,7 +80,7 @@ Giphy API Key:  fBAFRB0gejbg8XdW2IIRLUO9fyv95pI2
     </main>
     <footer>Made with angst by a struggling bootcamp student
 
-        <select name="Exlpore">
+        <select name="Explore">
             <option value="currentProjects">Boo!</option>
             <option value="currentProjects">Current Projects</option>
             <option value="spookyCreations">Spooky Creations</option>
@@ -651,5 +651,23 @@ body {
     /* -------------Footer--------------- */
     /* --------------------------------- */
 
-    
+
 //------------------------------------------------------------------------------------------------
+
+
+//--------------------- REQUIREMENTS: ------------------------------------------------
+// 1. Use the fetch API or Axios to communicate with an external web API. Use the data provided
+// by this API to populate your application’s content and features.
+// 2. Create user interaction with the API through a search feature, paginated gallery, or
+// similar. This feature should use GET requests to retrieve associated data.
+// 3. Make use of Promises and async/await syntax as appropriate.
+// 4. Organize your JavaScript code into at least three (3) different module files, and import
+// functions and data across files as necessary.
+// 5. Ensure the program runs as expected, without any undesired behavior caused by misunderstanding
+// of the JavaScript event loop (such as race conditions, API calls being handled out of order, etc.).
+// 6. Create an engaging user experience through the use of HTML and CSS.
+// 7. Ensure that the program runs without errors (comment out things that do not work, and explain
+// your blockers - you can still receive partial credit).
+// 8. Commit frequently to the git repository.
+// 9. Include a README file that contains a description of your application.
+// 10. Level of effort displayed in creativity, presentation, and user experience.
